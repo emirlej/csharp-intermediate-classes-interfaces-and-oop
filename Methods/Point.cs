7c13f55b04855ac7d960bb64782cbc538ed8723e
Emir Lejlic <emir.lej@gmail.com>
2018-10-30 13:32:58 +0100

    First commit of Methods

---
+++ Methods/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+            
+        // Initializer
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        // Method in class
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            this.X = newLocation.X;
+            this.Y = newLocation.Y;
+        }
+
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+        
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Testing out the class
+            var point = new Point(10, 20);
+            point.Move(new Point(40, 60));
+            Console.WriteLine($"Point is at {point.X}, {point.Y}");
+
+            // Second overload method
+            point.Move(100, 200);
+            Console.WriteLine($"Point is at {point.X}, {point.Y}");
+
+
+        }
+    }
+}
