9bb34b46891ff23887b2eb43ccfa008681330a59
Emir Lejlic <emir.lej@gmail.com>
2018-10-30 14:25:19 +0100

    Testing out how the readonly field works

---
+++ Fields/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields
+{
+    class Customer
+    {
+        public int Id;
+        public string Name;
+        // Readonly means you can assign it only once
+        public readonly List<Order> Orders = new List<Order>();
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            // This is a bug to exemplify readonly field
+            this.Orders = new List<Order>();
+        }
+    }
+}
---
+++ Fields/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(id: 1);
+
+            // Add orders
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            Console.WriteLine("Number of orders: {0}", customer.Orders.Count);
+
+            // This will not work, check Promote method
+            customer.Promote();
+        }
+    }
+}
