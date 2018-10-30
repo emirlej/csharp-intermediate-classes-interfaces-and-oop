6598c8e82f350c6d0779f993701b23e9ac1084a5
Emir Lejlic <emir@sherpaconsulting.onmicrosoft.com>
2018-10-30 10:28:18 +0100

    First section about constructors finished

---
+++ CsharpIntermediateCourse/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace CsharpIntermediateCourse
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        private List<Order> Orders { get; set; }
+
+        public Customer()
+        {
+            // Important to always initialize empty list when a parameter.
+            this.Orders = new List<Order>();
+        }
+
+        // ctor = contstructor shortcut
+        public Customer(int id) 
+            : this() // this() means calling the constructor above
+        {
+            this.Id = id;
+        }
+
+        // Overloading the clas
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ CsharpIntermediateCourse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpIntermediateCourse
+{
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1, "John");             
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+
+
+            // Best way to initialize classes
+            var customer2 = new Customer
+            {
+                Id = 1,
+                Name = "Joe Foo"
+            };
+
+
+
+
+        }
+    }
+}
